d5c63e257392bee0870a30cc72c29bf1c6f78398
kelvin <adelaar141@gmail.com>
2021-11-02 17:00:59 +0100

    laatste art toegevoegd

--- Assets/Scripts/Rings/RingSpawner.cs
+++ Assets/Scripts/Rings/RingSpawner.cs
@@ -9,8 +9,12 @@ namespace XRLab.VRoem.Core
     public class RingSpawner : MonoBehaviour
     {
         [SerializeField] private MoveObstacle _ring;
+        [SerializeField] private MoveObstacle _vuilnisbak;
+        [SerializeField] private MoveObstacle _spijkermat;
+        [SerializeField] private MoveObstacle _pion;
         [Range(1, 10)] [SerializeField] private float _spawnWidth;
         [Range(0.1f, 10)] [SerializeField] private float _spawnHeight;
+        float spawnnr;
 
         private void Start()
         {
@@ -24,7 +28,24 @@ namespace XRLab.VRoem.Core
             float randomHeight = Random.Range(-_spawnHeight / 2f, _spawnHeight / 2f) + transform.position.y;
             Vector3 spawnPosition = new Vector3(randomWidth, randomHeight, transform.position.z);
 
-            Instantiate(_ring, spawnPosition, transform.rotation, transform);
+            spawnnr = Random.Range(1 , 4);
+
+            switch (spawnnr)
+            {
+                default:
+                    Instantiate(_ring, spawnPosition, transform.rotation, transform);
+                    break;
+                case 1:
+                    Instantiate(_vuilnisbak, spawnPosition, transform.rotation, transform);
+                    break;
+                case 2:
+                    Instantiate(_pion, spawnPosition, transform.rotation, transform);
+                    break;
+                case 3:
+                    Instantiate(_spijkermat, spawnPosition, transform.rotation, transform);
+                    break;
+            }
+            
         }
 
         private void OnDrawGizmos()
