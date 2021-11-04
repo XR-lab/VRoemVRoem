46a31ee7177b4cddcebc5cd660947613160da1ac
Christian Donker <45289692+Christian199815@users.noreply.github.com>
2021-11-04 09:26:37 +0100

    First steps toward the tool
    tool made:
    - can push gameobject in to a list
    - can see how many triangles there are on a mesh

---
+++ Assets/Scripts/Environment/ChunkCreater.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkCreater : MonoBehaviour
+{
+
+
+
+    public int assetListSize = 10;
+
+    public List<GameObject> modelsToLoad = new List<GameObject>(20);
+
+   
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
