3731d8cbf6d271b3bf30c6a5a6e21370572447c9
xrlabRobin <89773055+MaxMellow99@users.noreply.github.com>
2021-11-05 13:41:06 +0100

    Development Merging
    Development Fixing Obstacle Interactions Added

--- Assets/Scripts/Core/Management/SpeedManager.cs
+++ Assets/Scripts/Core/Management/SpeedManager.cs
@@ -2,30 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpeedManager : MonoBehaviour
-{
+public class SpeedManager : MonoBehaviour {
     [SerializeField] private float _baseSpeed = 2;
     [SerializeField] private float _modifiedSpeed = 1;
+    [SerializeField] private float _roadSpeedMultiplier = 0.5f;
     [SerializeField] private float _accelerationLerpSpeed = 1;
+    [SerializeField] private float _accelerationLerpMultiplier = 1;
     [SerializeField] private float _currentMultiplier = 1;
 
     public float ModifiedSpeed { get { return _modifiedSpeed; } }
     public float CurrentMultiplier { get { return _currentMultiplier; } }
     private float _targetSpeed = 1;
+    private bool _overwritingControllerSpeed = false;
+    public bool OverwritingControllerSpeed { get { return _overwritingControllerSpeed; } }
 
-    private void Start()
-    {
+    private void Start() {
         _modifiedSpeed = _baseSpeed;
     }
 
-    public void CalculateModifedSpeed(float multiplier)
-    {
+    public void CalculateModifedSpeed(float multiplier) {
+        SetModifiedSpeed(_overwritingControllerSpeed ? _currentMultiplier : multiplier);
+
+        if (_overwritingControllerSpeed && _modifiedSpeed > _baseSpeed * _currentMultiplier - 0.1f) {
+            _overwritingControllerSpeed = false;
+        }
+    }
+
+    private void SetModifiedSpeed(float multiplier) {
         //Caluclate speed based on the base speed of the manager * number of the given multiplier (normally the car)
         _currentMultiplier = multiplier;
 
         _targetSpeed = _baseSpeed * multiplier;
 
         //Lerp speed to make the transition between speeds smooth
-        _modifiedSpeed = Mathf.Lerp(_modifiedSpeed, _targetSpeed, _accelerationLerpSpeed * Time.deltaTime);
+        _modifiedSpeed = Mathf.Lerp(_modifiedSpeed, _targetSpeed, _accelerationLerpSpeed * _accelerationLerpMultiplier * Time.deltaTime);
+
+        if ((_modifiedSpeed > _baseSpeed || multiplier > 1.5f) && !_overwritingControllerSpeed) {
+            _accelerationLerpMultiplier = 1;
+        }
+    }
+
+    public void OverrideControllerSpeed(float multiplier, float slowerAccelMultiplier) {
+        _overwritingControllerSpeed = true;
+        _accelerationLerpMultiplier = slowerAccelMultiplier;
+        SetModifiedSpeed(multiplier);
+    }
+
+    public void LoseAllSpeed(float slowerAccelMultiplier) {
+        _modifiedSpeed = 0;
+        _accelerationLerpMultiplier = slowerAccelMultiplier;
     }
 }
