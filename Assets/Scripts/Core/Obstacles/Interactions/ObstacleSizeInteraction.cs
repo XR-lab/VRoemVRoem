fb3a4995506c046241e72a313ad56f9cbb44d338
Richard Ram <r.ram@ma-web.nl>
2021-11-17 13:41:49 +0100

    Cleanup on scripts. Added TODOs for refactoring

--- Assets/Scripts/Core/Obstacles/Interactions/ObstacleSizeInteraction.cs
+++ Assets/Scripts/Core/Obstacles/Interactions/ObstacleSizeInteraction.cs
@@ -2,18 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//TODO: fix code conventions, namespace.
 public class ObstacleSizeInteraction : MonoBehaviour
 {
     [SerializeField] private bool BigObject;
     [SerializeField] private Collider colider;
 
-    protected void Interact(Collision collision) {
-       
-        if (BigObject) {
-            print("hit");
-            MoneySystem.currentMonney = MoneySystem.currentMonney - MoneySystem.MonneyToLose;
-            this.gameObject.GetComponent<Collider>().isTrigger = true;
-            colider.isTrigger = true;
-        }
+    protected void Interact(Collision collision)
+    {
+        if (!BigObject) return;
+        
+        print("hit");
+        MoneySystem.currentMonney = MoneySystem.currentMonney - MoneySystem.MonneyToLose;
+        this.gameObject.GetComponent<Collider>().isTrigger = true;
+        colider.isTrigger = true;
     }
 }
--- Assets/Scripts/Core/Obstacles/ObstacleTrigger.cs
+++ Assets/Scripts/Core/Obstacles/ObstacleTrigger.cs
@@ -6,30 +6,19 @@ namespace XRLab.VRoem.Core
 {
     public class ObstacleTrigger : MonoBehaviour
     {
-        [SerializeField] private Renderer _renderer;
         private ObjectHitTracker _hitTracker;
 
         private void Start()
         {
             _hitTracker = FindObjectOfType<ObjectHitTracker>();
         }
-
-        //Set the color to green when hitting the player
-        private void OnTriggerEnter(Collider other) {
-            if (other.attachedRigidbody.CompareTag(Tags.PLAYER)) {
-                print("HIT");
-
-                //_renderer.material.SetColor("_Color", Color.green);
+        
+        private void OnTriggerEnter(Collider other) 
+        {
+            if (other.attachedRigidbody.CompareTag(Tags.PLAYER)) 
+            {
                 _hitTracker.objectHitCounter += 1;
             }
         }
-        //private void OnCollisionEnter(Collision collision) 
-        //{
-        //    if (collision.gameObject.CompareTag(Tags.PLAYER)) {
-        //        Debug.LogError(collision.gameObject.tag);
-        //        //_renderer.material.SetColor("_Color", Color.green);
-        //        //FindObjectOfType<ObjectHitTracker>().objectHitCounter += 1;
-        //    }
-        //}
     }
 }
