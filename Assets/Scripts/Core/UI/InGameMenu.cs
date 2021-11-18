3e96e85cf5749ee6108c7787fa5e275b9d8887e8
Algemeen account XR lab <xrlab.nl@gmail.com>
2021-11-18 10:57:27 +0100

    eindschrem/ dublicate script verwijdert

--- Assets/Scripts/Core/UI/InGameMenu.cs
+++ Assets/Scripts/Core/UI/InGameMenu.cs
@@ -5,92 +5,84 @@ using UnityEngine.SceneManagement;
 using UnityEngine.XR;
 using TMPro;
 
-
-public class InGameMenu : MonoBehaviour
+namespace XRLab.VRoem.core
 {
-    
-    [SerializeField] private GameObject pauzeMenu;
-    [SerializeField] private GameObject deadMenu;
-    [SerializeField] private GameObject finishMenu;
-
-    [SerializeField] private TMP_Text MoneyGot;
-    [SerializeField] private TMP_Text Moneyearnd;
-
-
-
-
-    //check input 
-    void Start()
+    public class InGameMenu : MonoBehaviour
     {
-        pauzeMenu.SetActive(false);
-        deadMenu.SetActive(false);
-        finishMenu.SetActive(false);
-    }
+
+        [SerializeField] private GameObject pauzeMenu;
+        [SerializeField] private GameObject deadMenu;
+        [SerializeField] private GameObject finishMenu;
+
+        [SerializeField] private TMP_Text MoneyGot;
+        [SerializeField] private TMP_Text Moneyearnd;
 
 
-    //restart press space
-    void Update()
-    {
-        if (Input.GetKeyDown("space"))
-        {
-            RestartLevel();
+
+
+        //check input 
+        void Start() {
+            pauzeMenu.SetActive(false);
+            deadMenu.SetActive(false);
+            finishMenu.SetActive(false);
         }
 
-    } 
-    
-    //check if pauzemenu is active
-    void CheckOpen()
-    {
-        if (!pauzeMenu.activeInHierarchy)
-        {
-            Open();
+
+        //restart press space
+        void Update() {
+            if (Input.GetKeyDown("space")) {
+                RestartLevel();
+            }
+
         }
-        else if (pauzeMenu.activeInHierarchy)
-        {
-            close();
+
+        //check if pauzemenu is active
+        void CheckOpen() {
+            if (!pauzeMenu.activeInHierarchy) {
+                Open();
+            } else if (pauzeMenu.activeInHierarchy) {
+                close();
+            }
         }
-    }
 
-    // call to restart level
-    public void RestartLevel()
-    {
-        Time.timeScale = 1;
-       
-        SceneManager.LoadScene(0);
-    }
+        // call to restart level
+        public void RestartLevel() {
+            Time.timeScale = 1;
 
-    public void PauzeMenuButton() {
-        if (pauzeMenu.activeInHierarchy) {
-            close();
-        } else if (!pauzeMenu.activeInHierarchy) {
-            Open();
+            SceneManager.LoadScene(0);
         }
-    }
-   
-    //open pauze menu and stop game time
-    public void Open()
-    {
-        Time.timeScale = 0;
-        pauzeMenu.SetActive(true);
-        Debug.Log("open");
-        
-    }
-    //close pauze menu and start game time
-    public void close()
-    {
-        Time.timeScale = 1;
-        pauzeMenu.SetActive(false);
-        Debug.Log("close");
-    }
 
-    public void BackToMainMenu() {
-        SceneManager.LoadScene(1);
-    }
+        public void PauzeMenuButton() {
+            if (pauzeMenu.activeInHierarchy) {
+                close();
+            } else if (!pauzeMenu.activeInHierarchy) {
+                Open();
+            }
+        }
 
-    public void ActivateFinsishMenu() {
+        //open pauze menu and stop game time
+        public void Open() {
+            Time.timeScale = 0;
+            pauzeMenu.SetActive(true);
+            Debug.Log("open");
 
-        MoneyGot.text = "Your money: " + MoneySystem.currentMonney.ToString();
-        Moneyearnd.text = "Earnd money: " + MoneySystem.TotalMoney.ToString(); 
+        }
+        //close pauze menu and start game time
+        public void close() {
+            Time.timeScale = 1;
+            pauzeMenu.SetActive(false);
+            Debug.Log("close");
+        }
 
+        public void BackToMainMenu() {
+            SceneManager.LoadScene(1);
+        }
+
+        public void ActivateFinsishMenu() {
+
+            MoneyGot.text = "Your money: " + MoneySystem.currentMonney.ToString();
+            Moneyearnd.text = "Earnd money: " + MoneySystem.TotalMoney.ToString();
+
+        }
     }
 }
