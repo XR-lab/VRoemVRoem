5195f219028a70c4b565aa6e52336f5f202b104c
MaxMellow99 <apenstaartje.max@gmail.com>
2021-11-19 10:40:40 +0100

    Script Added
    Script Added

---
+++ Assets/Scripts/Vehicle/CarParticleInteractions.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace XRLab.VRoem.Vehicle
+{
+    public class CarParticleInteractions : MonoBehaviour
+    {
+        private void OnCollisionEnter(Collider other)
+        {
+            
+        }
+    }
+}
