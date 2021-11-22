59cfbc4ea5677a829e13c02430ae56f78768c788
JordanG <26260@ma-web.nl>
2021-11-22 13:49:50 +0100

    Invincibility added

--- Assets/Scripts/Vehicle/CarDamage.cs
+++ Assets/Scripts/Vehicle/CarDamage.cs
@@ -9,11 +9,15 @@ public class CarDamage : MonoBehaviour
 {
     [SerializeField] private float _slowerAccelMultiplier = 0.5f;
     [SerializeField] private float _loseControlTime = 1;
+    [SerializeField] private float _invincibilityTime = 2;
     private SimpleMovementCar _carMovement;
 
     [SerializeField] public GameObject _hitParticle;
     [SerializeField] public GameObject _cashParticle;
-    
+    [SerializeField] public GameObject _carModel;
+
+    private bool _invincible = false;
+
     void Start()
     {
         _carMovement = GetComponent<SimpleMovementCar>();
@@ -21,11 +25,20 @@ public class CarDamage : MonoBehaviour
 
     public void Damage()
     {
+        if (_invincible)
+        {
+            return;
+        }
+
         _carMovement.CanMove = false;
         _carMovement.GetSpeedManager.LoseAllSpeed(_slowerAccelMultiplier);
         _carMovement.GetRigidbody.velocity = Vector3.zero;
 
         Invoke(nameof(ReturnControl), _loseControlTime);
+        Invoke(nameof(MakeVunerable), _invincibilityTime);
+
+        _invincible = true;
+        StartCoroutine(nameof(BlinkCar));
 
         if (_hitParticle == null || _cashParticle == null)
         {
@@ -42,6 +55,22 @@ public class CarDamage : MonoBehaviour
     private void ReturnControl()
     {
         _carMovement.CanMove = true;
+    }
+
+    private void MakeVunerable()
+    {
+        _invincible = false;
+    }
+
+    private IEnumerator BlinkCar()
+    {
+        while(_invincible)
+        {
+            _carModel.SetActive(false);
+            yield return new WaitForSeconds(0.2f);
+            _carModel.SetActive(true);
+            yield return new WaitForSeconds(0.2f);
+        }
     }
 
     IEnumerator ParticleDeactivate()
